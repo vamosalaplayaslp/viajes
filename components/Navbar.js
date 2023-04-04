ac98adaca69f4041ee20d34817fcedf145899628
Omar Barbosa <omarbarbosa06@gmail.com>
2023-04-04 15:26:33 -0600

    Header and hero

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,42 +1,67 @@
 import React, { useState } from 'react'
 
 const MENU_LIST = [
-  { text: 'Home', href: '/' },
-  { text: 'About Us', href: '/about' },
-  { text: 'Contact', href: '/contact' }
+  { text: 'Inicio', href: '/' },
+  { text: '¿Quiénes somos?', href: '/about' },
+  { text: 'Contacto', href: '/contact' }
 ]
 const Navbar = () => {
   const [navActive, setNavActive] = useState(null)
   const [activeIdx, setActiveIdx] = useState(-1)
 
   return (
-    <header>
-      <nav className={`nav`}>
-        <img src="logo.png" alt="" width={140} height={70} />
+    <>
+      <section className="section-header">
+        <header>
+          <nav className={`nav`}>
+            <img src="logo.png" alt="" width={140} height={70} />
 
-        <div
-          onClick={() => setNavActive(!navActive)}
-          className={`nav__menu-bar`}
-        >
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <div className={`${navActive ? 'active' : ''} nav__menu-list`}>
-          {MENU_LIST.map((menu, idx) => (
             <div
-              onClick={() => {
-                setActiveIdx(idx)
-                setNavActive(false)
-              }}
-              key={menu.text}
+              onClick={() => setNavActive(!navActive)}
+              className={`nav__menu-bar`}
             >
-              <a href="/">hola</a>
+              <div className={`${navActive ? 'is-rotated' : ''}`}></div>
+              <div className={`${navActive ? 'is-gone' : ''}`}> </div>
+              <div className={`${navActive ? 'is-rotated2' : ''}`}></div>
             </div>
-          ))}
-        </div>
-      </nav>
-    </header>
+            <div className={`${navActive ? 'active' : ''} nav__menu-list`}>
+              {MENU_LIST.map((menu, idx) => (
+                <div
+                  onClick={() => {
+                    setActiveIdx(idx)
+                    setNavActive(false)
+                  }}
+                  key={menu.text}
+                >
+                  <a href="/">{menu.text}</a>
+                </div>
+              ))}
+            </div>
+          </nav>
+        </header>
+        <section className="hero">
+          <div className="left">
+            <div className="pre-header">Los mejores destinos de mexico</div>
+            <div className="title">
+              <h1>
+                Viaja, disfruta, y ten las mejores vacaciones con tu agencia de
+                confiaza
+                <span> Vamos a la playa</span>
+              </h1>
+            </div>
+            <div className="buttons">
+              <button>Conoce nuestros viajes</button>
+            </div>
+          </div>
+          <div className="right">
+            <img
+              src="https://uploads-ssl.webflow.com/6170700f1a1db6b3a0ac16ed/617938cf58e87917dc7a61ec_woman-planes.png"
+              alt=""
+            />
+          </div>
+        </section>
+      </section>
+    </>
   )
 }
 
